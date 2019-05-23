96df98208001ebbff7dede186abc497b2768fdad
Mengqing Wu <mengqing.wu@desy.de>
2019-05-23 21:20:51 +0200

    finalize the exploring!

--- explorer.py
+++ explorer.py
@@ -2,31 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import pandas as pd
+import argparse
 
-# Fixing random state for reproducibility
-#np.random.seed(19680801)
-
-#mu, sigma = 100, 15
-#x = mu + sigma * np.random.randn(10000)
-#print(type(x))
-# # the histogram of the data
-# n, bins, patches = plt.hist(x, 50, density=True, facecolor='g', alpha=0.75)
-
+kpix_name = 'runtime_full_ns'
+tlu_name = 'timestamp_64' #'timestamp_low'
 ####
 #kpix = pd.read_csv("dumpkpix_trigger.csv")
 kpix = pd.read_csv("Run_20190505_111442.csv")
+#tlu = pd.read_csv("run255_trigger.csv")
+tlu = pd.read_csv("run255_ts64.csv")
+
+#print(kpix)
+
 #print(kpix.head())
 #print(kpix.info())
-trigs = len(kpix.index)
-print (' Kpix recorded %i Triggers' % trigs)
-#kpix = kpix.iloc[0:100]
-tskpix = np.array(kpix['TrigTimestamp_ns'])
-
-
-tlu = pd.read_csv("run255_trigger.csv")
 #print(tlu.head())
+print (' Kpix recorded %i Triggers, while tlu recorded %i Triggers' % (len(kpix.index), len(tlu.index)))
+
+kk = len(kpix.index)
+tt = len(tlu.index)
+trigs = kk if kk<tt else tt
+#trigs = 150
+
+kpix = kpix.iloc[0:trigs]
+tskpix = np.array(kpix[kpix_name])
+
 tlu = tlu.iloc[0:trigs]
-tstlu = np.array(tlu['timestamp_low'])
+tstlu = np.array(tlu[tlu_name])
 #print (tstlu)
 #tlu['timestamp_low'].plot(kind='hist', bins=10)
 #plt.xlabel('timestamp [ns]')
@@ -34,7 +36,8 @@ tstlu = np.array(tlu['timestamp_low'])
 
 
 diffs = tskpix - tstlu
-print(diffs)
+print(diffs[:200])
+
 xmin = np.amin(diffs)-10
 xmax = np.amax(diffs)+10
 if (xmax-xmin)%5!=0:
